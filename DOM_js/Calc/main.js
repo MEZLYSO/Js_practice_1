6eca06b3b3200813d638318eebd10f8d47b274dd
Maury <lynuxazura@gmail.com>
2025-02-19 04:30:07 -0600

    Final
    Pre-final concept fo Calc

--- DOM_js/Calc/main.js
+++ DOM_js/Calc/main.js
@@ -1,17 +1,26 @@
 //Elementos basicos
 const root = document.getElementById("root")
+document.getElementById("input_data").disabled = true;
+document.getElementById("output_data").disabled = true;
 root.append(document.createElement("br"))
 const pad = document.createElement("div")
+pad.id = "pad"
 root.append(pad)
 const operands = ["+","-","/","*"]
 
 function resolve(prompt){
-  return eval(prompt)
+  let res = eval(prompt)
+  if (isNaN(res)) {
+    return "Operacion no soportada"
+  }else{
+    return res
+  }
 }
 
 //Boton CA
 const ca_button = document.createElement("button")
 ca_button.textContent = "CA";
+ca_button.id = "ca_button"
 ca_button.addEventListener("click",()=>{
   document.getElementById("input_data").value = "" 
   document.getElementById("output_data").value = "" 
@@ -40,7 +49,7 @@ for (let i = 0; i < 10; i++) {
     const input = document.getElementById("input_data")
     const output = document.getElementById("output_data")
     input.value += `${i}`
-    output.value = resolve(input.value)
+    output.value = `= ${resolve(input.value)}`
   });
   pad.append(button)
   
