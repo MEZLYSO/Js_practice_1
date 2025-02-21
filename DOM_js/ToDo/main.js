6fa655a77bf463784fab8220aa1e9c0fc013e53d
Maury <lynuxazura@gmail.com>
2025-02-20 21:38:01 -0600

    Pseudo Final

--- DOM_js/ToDo/main.js
+++ DOM_js/ToDo/main.js
@@ -1,15 +1,32 @@
 const root = document.getElementById("root");
 const button = document.getElementById("save")
-let task = []
+const button1 = document.getElementById("clean")
 
 button.addEventListener("click",()=>{
   const input = document.getElementById("task")
-  task.push({title:input.value});
-  const div = document.getElementById("root")
+  const div = document.getElementById("root") 
   const task_div = document.createElement("div")
+  const button_delete = document.createElement("button")
+  button_delete.textContent = "Completada"
+  button_delete.id = "btn_delete"
+  if(input.value==""){
+    return
+  }
   task_div.textContent = `${input.value}`
+  task_div.id = `${input.value}`
+  task_div.className = "task_div"
+  input.value = ""
+  button_delete.addEventListener("click",()=>{
+    task_div.remove()
+  });
+  task_div.append(button_delete)
   div.append(task_div)
 });
+
+button1.addEventListener("click", () => {
+  const elements = document.querySelector(".task_div")
+  elements.remove()
+})
 
 
 
